8c31f8ae2271c1506130c39c26d1fe077babfef7
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-09 23:57:01 +0000

    added POST /urls

--- express_server.js
+++ express_server.js
@@ -4,9 +4,21 @@ const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const generateRandomString = function() {
+  let text = '';
+  let caracters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let car of caracters) {
+    text += caracters.charAt(Math.floor(Math.random() * 6))
+    return text;
+  }
 };
 
 app.get("/", (req, res) => {
@@ -26,11 +38,21 @@ app.get('/urls', (req,res) => {
   res.render('urls_index', templateVars)
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL
   const templateVars = { shortURL, longURL: urlDatabase[shortURL] };
   res.render('urls_show', templateVars);
 });
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
+})
+
 
 
 
