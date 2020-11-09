91e43a851ea79d586a2745b8f590c1458dbf73da
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-09 19:34:10 +0000

    Added HTML code

--- express_server.js
+++ express_server.js
@@ -11,6 +11,14 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase); // {"b2xVn2":"http://www.lighthouselabs.ca","9sm5xK":"http://www.google.com"} = JSON string representing the entire urlDatabase object
+});
+
+app.get('/hello', (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n")
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
